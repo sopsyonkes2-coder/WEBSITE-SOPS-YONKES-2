688b60517dd8eabc8af3dbcdcd98ade10d158a6b
Deva <Sopsyonkes2@gmail.com>
2026-06-05 20:19:03 +0700

    Fix DokumenPage dengan mengganti komponen ke div sementara

--- src/page.tsx
+++ src/page.tsx
@@ -1,8 +1,13 @@
 import { getDocuments } from "@/services/documentService";
-import DocumentClient from "./DocumentClient";
 
 export default async function DokumenPage() {
   const documents = await getDocuments();
 
-  return <DocumentClient documents={documents} />;
+  // Kita kembalikan teks sederhana agar tidak error saat build
+  return (
+    <div>
+      <h1>Daftar Dokumen</h1>
+      <pre>{JSON.stringify(documents, null, 2)}</pre>
+    </div>
+  );
 }
